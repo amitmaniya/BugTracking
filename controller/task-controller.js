6f417ba6268388f2b7afb7d92b5fb9a234b90204
amitmaniya <99233672+amitmaniya@users.noreply.github.com>
2022-02-20 23:32:09 +0530

    Update task-controller.js

--- controller/task-controller.js
+++ controller/task-controller.js
@@ -47,7 +47,7 @@ module.exports.getAlltask = function (req, res) {
 }
 
 //delete
-module.exports.deleteTask = function(req,res){
+module.exports.deletetask = function(req,res){
     //params userid 
     let taskId = req.params.taskId //postman -> userid 
 
